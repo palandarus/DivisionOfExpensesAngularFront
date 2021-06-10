e1ad4b8d71fcb373af634ebd5f2d5d5cab9fc6f9
palandarus <klarkken@yandex.ru>
2021-06-10 15:48:10 +0300

    deploying on heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import {Environment} from './interface';
 export const environment: Environment = {
   production: true,
   apiKey: 'AIzaSyBeVNY1dO6dHpkj5e7EOZU2ctA4O-chAvQ',
-  API_ENDPOINT: 'http://localhost:8189'
+  API_ENDPOINT: 'https://divisionofexpenses.herokuapp.com'
 };
