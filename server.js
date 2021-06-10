daa6f293dafdad7ef169c2b728be2356f0e5226b
palandarus <klarkken@yandex.ru>
2021-06-10 15:58:30 +0300

    deploying on heroku

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const app=express();
-app.use(express.static(__dirname + 'dist/angular-blog'));
-app.get('/*', function (req,res) {
-    res.sendFile(path.join(__dirname + 'dist/angular-blog/index.html'));});
-app.listen(process.env.PORT || 8080)
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Express server listening on port', port)
+});
