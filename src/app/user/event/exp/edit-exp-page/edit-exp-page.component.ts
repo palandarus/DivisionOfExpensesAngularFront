6bcce4e64b854adefb426b6288317183e83ca88e
palandarus <klarkken@yandex.ru>
2021-06-13 13:05:21 +0300

    added expense add page

--- src/app/user/event/exp/edit-exp-page/edit-exp-page.component.ts
+++ src/app/user/event/exp/edit-exp-page/edit-exp-page.component.ts
@@ -34,79 +34,16 @@ export class EditExpPageComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.params
-        .pipe(
-            switchMap((params: Params) => {
-              console.log('params.id ', params.id);
-              return this.expensesService.getById(params.id);
-            })
-        ).subscribe((expense: Expense) => {
-      this.expense = expense;
-      this.directPayerMap = this.expense.directPayerMap;
-      this.partitialPayerMap = this.expense.partitialPayerMap;
-      console.log('directPayerMap was loaded ', this.directPayerMap);
-      console.log('partitialPayerMap was loaded ', this.partitialPayerMap);
-      this.form = new FormGroup({
-        comment: new FormControl(expense.comment, Validators.required),
-        date: new FormControl(expense.expenseDate, Validators.required),
-        selectedBuyer: new FormControl(this.expense.buyer),
-        totalExpenseSum: new FormControl(this.expense.totalExpenseSum, [Validators.required, Validators.pattern(/^[.\d]+$/)]),
-        newDirectUser: new FormControl(null, Validators.required),
-        newDirectSum: new FormControl(null, [Validators.required, Validators.pattern(/^[.\d]+$/)]),
-        newPartitialUser: new FormControl(null, Validators.required),
-        newPartitialCoef: new FormControl(null, [Validators.required, Validators.pattern(/^[.\d]+$/)])
-      });
-    });
+
   }
 
   submit() {
-    const expense: Expense = {
-      totalExpenseSum: this.form.value.totalExpenseSum,
-      comment: this.form.value.comment,
-      event: this.form.value.event,
-      expenseDate: new Date(this.form.value.date),
-      buyer: this.form.value.selectedBuyer,
-      directPayerMap: this.directPayerMap,
-      partitialPayerMap: this.partitialPayerMap
-    };
 
-    this.expensesService.update(expense).subscribe(() => {
-      this.form.reset();
-      this.alert.success('Трата была успешно обновлена.');
-    });
+
+
   }
 
 
 
-  addDirectPayer(newDirectUser: string, newDirectSum: string) {
-    this.directPayerMap.set(newDirectUser, newDirectSum);
-    this.form.get('newDirectUser').reset();
-    this.form.get('newDirectSum').reset();
-
-  }
-
-  addPartitialPayer(newPartitialUser: string, newPartitialCoef: string) {
-    this.partitialPayerMap.set(newPartitialUser, newPartitialCoef);
-    this.form.get('newPartitialUser').reset();
-    this.form.get('newPartitialCoef').reset();
-  }
-
-
-  removeDirectPayer(directUser: string) {
-    this.partitialPayerMap.delete(directUser);
-    console.log(this.partitialPayerMap);
-  }
-
-  removePartitialPayer(partitialUser: string) {
-    this.partitialPayerMap.delete(partitialUser);
-    console.log(this.partitialPayerMap);
-  }
-  convertDirectPayerMapToJson(directPayerMap: Map<string, string>) {
-    directPayerMap.forEach(((value, key) => (this.directPayerMapJson)[key] = value));
-  }
-
-  convertPartitialPayerMapToJson(partitialPayerMap: Map<string, string>) {
-    partitialPayerMap.forEach(((value, key) => (this.partitialPayerMapJson)[key] = value));
-  }
 
 }
